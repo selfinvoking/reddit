ae161290b6dd1e806d113c5a17876f5916a2a125
Carl Walsh <carl@selfinvoking.com>
2022-01-19 21:55:41 +0000

    remove old output.csv and fix typo

--- helpers.py
+++ helpers.py
@@ -1,5 +1,27 @@
 import datetime
 
+# Get a list of submission ids that were approved
+# in the last three months
+def get_approved_ids(reddit):
+    approved = {}
+
+    for action in reddit.subreddit('learnjapanese').mod.log(action='approvelink', limit=99999):
+        if action.target_fullname != None:
+            approved[action.target_fullname] = action.created_utc
+    
+    return approved
+
+# Get a list of submission ids that were approved
+# in the last three months
+def get_removed_ids(reddit):
+    removed = {}
+
+    for action in reddit.subreddit('learnjapanese').mod.log(action='removelink', limit=99999):
+        if action.target_fullname != None:
+            removed[action.target_fullname] = action.created_utc
+    
+    return removed
+
 # Get all post approvals/removals from the mod log
 # Note: limited by the 3 month cap on the mod log
 # Todo: Might be possible to use the pushshift api work around this limit
@@ -7,7 +29,7 @@ def get_actioned_submissions(reddit):
     actioned_ids = {}
     actioned_posts = []
 
-    for action_type in ['approvelink' 'removedlink']:
+    for action_type in ['approvelink' 'removelink']:
         for action in reddit.subreddit('learnjapanese').mod.log(action=action_type, limit=99999):
             if action.target_fullname != None:
                 actioned_ids[action.target_fullname] = action.created_utc
